7f907808d2c11c4904546998e5ffe5e828423a9f
mariadhilla <dhillamaria@gmail.com>
2023-08-16 21:11:03 -0700

    experiment

---
+++ sketch.js
@@ -0,0 +1,40 @@
+let x = [],
+  y = [],
+  segNum = 300,
+  segLength = 1;
+
+for (let i = 0; i < segNum; i++) {
+  x[i] = 0;
+  y[i] = 0;
+}
+
+function setup() {
+  createCanvas(710, 400);
+  strokeWeight(10);
+  stroke('rgba(68, 64, 255, 0.75)');
+}
+
+function draw() {
+  background('#fafafa');
+  dragSegment(0, mouseX, mouseY);
+  for (let i = 0; i < x.length - 1; i++) {
+    dragSegment(i + 1, x[i], y[i]);
+  }
+}
+
+function dragSegment(i, xin, yin) {
+  const dx = xin - x[i];
+  const dy = yin - y[i];
+  const angle = atan2(dy, dx);
+  x[i] = xin - cos(angle) * segLength;
+  y[i] = yin - sin(angle) * segLength;
+  segment(x[i], y[i], angle);
+}
+
+function segment(x, y, a) {
+  push();
+  translate(x, y);
+  rotate(a);
+  line(0, 0, segLength, 0);
+  pop();
+}
